7b1382d0aa5d07e3a2426b0bda43fbad2a6090a1
Marianne <105245058+smariannee@users.noreply.github.com>
2024-04-17 08:49:25 -0600

    Add files via upload

---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="home">
+        <h1>Login con oauth</h1>
+        <div v-if="!$auth.loading">
+            <button v-if="!$auth.isAuthenticated" @click="login">Log in</button>
+            <button v-if="$auth.isAuthenticated" @click="logout">Log out</button>
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: "home",
+
+    methods: {
+        login() {
+            this.$auth.loginWithRedirect();
+        },
+        logout() {
+            this.$auth.logout({
+                logoutParams: {
+                    returnTo: window.location.origin,
+                },
+            });
+        },
+    },
+};
+</script>
